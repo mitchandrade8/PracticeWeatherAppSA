5eb5b47f7b08b9c2d1a51dc60e3c615e8928b7e1
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-23 13:07:26 -0600

    Initial Commit

---
+++ PracticeWeatherAppSA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeWeatherAppSA
+//
+//  Created by Mitch Andrade on 9/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeWeatherAppSA/PracticeWeatherAppSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeWeatherAppSAApp.swift
+//  PracticeWeatherAppSA
+//
+//  Created by Mitch Andrade on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeWeatherAppSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
