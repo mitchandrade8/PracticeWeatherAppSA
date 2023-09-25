5d836d8a53c499bfec0ee4afa7f2a50445532a26
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-24 21:51:34 -0600

    --Progress: Build out UI to look professional and simple, then start to refactor/extract subview to clean the code and increase readability, makes the project and code super clean

--- PracticeWeatherAppSA/ContentView.swift
+++ PracticeWeatherAppSA/ContentView.swift
@@ -24,49 +24,32 @@ struct ContentView: View {
                         .foregroundColor(.white)
                         .padding(.horizontal, 80)
                     
-                    VStack(spacing: 8) {
-                        Image(systemName: "cloud.sun.fill")
-                            .renderingMode(.original)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 220, height: 220)
-                        
-                        Text("76°")
-                            .font(.system(size: 70, weight: .medium, design: .serif))
-                            .foregroundColor(.white)
-                        
-            
-                        HStack(spacing: 20) {
-                            DailyTemperatureSnapshotView(day: "Mon", temperatureImage: "cloud.sun.rain.fill", dailyTemperature: "58")
-                            DailyTemperatureSnapshotView(day: "Tue", temperatureImage: "cloud.sun.fill", dailyTemperature: "76")
-                            DailyTemperatureSnapshotView(day: "Wed", temperatureImage: "sun.max.fill", dailyTemperature: "94")
-                            DailyTemperatureSnapshotView(day: "Thr", temperatureImage: "cloud.sun.fill", dailyTemperature: "87")
-                            DailyTemperatureSnapshotView(day: "Fri", temperatureImage: "sun.max.fill", dailyTemperature: "80")
-                        }
-                        .padding(.top, 40)
-                        
-                        
-                        Spacer()
-                        
-                        Button {
-                            print("Button tapped!")
-                        } label: {
-                            Text("Light Mode")
-                                .frame(width: 280, height: 50)
-                                .background(.white)
-                                .font(.system(size: 20, weight: .bold, design: .default))
-                                .cornerRadius(8)
-                        }
-                        
-                        
-                        Spacer()
-                        
+                    MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 76)
+                    
+                    HStack(spacing: 20) {
+                        DailyTemperatureSnapshotView(day: "Mon", temperatureImage: "cloud.sun.rain.fill", dailyTemperature: "58")
+                        DailyTemperatureSnapshotView(day: "Tue", temperatureImage: "cloud.sun.fill", dailyTemperature: "76")
+                        DailyTemperatureSnapshotView(day: "Wed", temperatureImage: "sun.max.fill", dailyTemperature: "94")
+                        DailyTemperatureSnapshotView(day: "Thr", temperatureImage: "cloud.sun.fill", dailyTemperature: "87")
+                        DailyTemperatureSnapshotView(day: "Fri", temperatureImage: "sun.max.fill", dailyTemperature: "80")
                     }
+                    .padding(.top, 40)
+                    
                     
                     Spacer()
+                    
+                    Button {
+                        print("Button tapped!")
+                    } label: {
+                        Text("Light Mode")
+                            .frame(width: 280, height: 50)
+                            .background(.white)
+                            .font(.system(size: 20, weight: .bold, design: .default))
+                            .cornerRadius(8)
+                    }
+                    Spacer()
+                    
                 }
-                
-                
                 
             }
             
@@ -122,5 +105,28 @@ struct CityTextView: View {
         Text(cityName)
             .font(.system(size: 32, weight: .medium, design: .default))
             .foregroundColor(.white)
+    }
+}
+
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 220, height: 220)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium, design: .serif))
+                .foregroundColor(.white)
+           
+            
+        }
     }
 }
